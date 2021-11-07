4d28a656c3f25a318a1fb63381dfe4083b8fe6f6
Joshua Garcia <garciajoshua907@yahoo.com>
2021-11-06 23:47:05 -0600

    replaces like,commen,share <i> with svg images and linked like button to add of subtract a like to each single post

--- scripts.js
+++ scripts.js
@@ -1,42 +1,79 @@
 $(document).ready(function () {
-  const address = "https://randomuser.me/api/?results=20";
-  async function grabUsers(call) {
-    const people = await fetch(call).then((res) => res.json());
-    people.results.forEach((person) => {
-      const newLi = `<li><img src=${person.picture.medium}><img></li> `;
-      $("#img-display").append(newLi);
-    });
-    let numberRefrence = people.results.length;
-    console.log(people.results);
-    //do while numbref>0 produce a
-    //post with user-img text and comments util section
-    // Math.floor(Math.random() * numberRefrence);
-    while (numberRefrence > 0) {
-      console.log("create");
-      let randomUser = randomNumber(numberRefrence);
-      let selectedPerson = people.results[randomUser];
-      const newPost = `
-      <div id="single-post">
-        <div class="post-heading">
-          <img src=${selectedPerson.picture.medium}></img>
-          <span>${selectedPerson.name.first} ${selectedPerson.name.last}</span>
-        </div>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, aliquam perspiciatis architecto neque deleniti </p>
-        <div id=post-interactions> ${randomNumber(50)} likes ~ ${randomNumber(
-        30
-      )} shares<div>
-      </div>`;
-      $(".post-section").append(newPost);
-      numberRefrence--;
-    }
-  }
+  const address = "https://randomuser.me/api/?results=12";
+  grabUsers(address);
 
   $("#next-scoll").click(() => {
     alert("hit");
   });
-
-  grabUsers(address);
 });
+async function grabUsers(call) {
+  const people = await fetch(call).then((res) => res.json());
+  people.results.forEach((person) => {
+    const newLi = `<li><img src=${person.picture.medium}><img></li> `;
+    $("#img-display").append(newLi);
+  });
+  let numberRefrence = people.results.length;
+
+  //do while numbref>0 produce a
+  //post with user-img text and comments util section
+  // Math.floor(Math.random() * numberRefrence);
+  while (numberRefrence > 0) {
+    let randomUser = randomNumber(numberRefrence);
+    let selectedPerson = people.results[randomUser];
+
+    const newPost = `
+    <div class="single-post">
+      <div class="post-heading">
+        <img src=${selectedPerson.picture.medium}></img>
+        <span>${selectedPerson.name.first} ${selectedPerson.name.last}</span>
+      </div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, aliquam perspiciatis architecto neque deleniti </p>
+    ${interactionBar()}
+    
+    </div>`;
+    $(".post-section").append(newPost);
+    numberRefrence--;
+  }
+  interactionListener();
+}
 function randomNumber(limmitNum) {
   return Math.floor(Math.random() * limmitNum);
+}
+
+function interactionBar() {
+  return `<div class="post-interactions"> <small class='likes'>${randomNumber(
+    50
+  )}</small> likes ~ <small class='shares'>${randomNumber(30)}</small> shares
+<ol class="interaction-bar">
+ <li class="like"></li>
+ <li class="comment"></li>
+ <li class="share"></li>
+  </ol>
+</div>`;
+}
+function interactionListener() {
+  $(".interaction-bar").click((e) => {
+    let action = e.target;
+
+    if ($(action).hasClass("like")) {
+      const hasClicked = action.className;
+      let stringNubmer = $(action).parent().parent().children(".likes")[0];
+      switch (hasClicked) {
+        case "like":
+          {
+            $(action).addClass("clicked");
+            const number = parseInt(stringNubmer.innerHTML);
+            stringNubmer.innerHTML = number + 1;
+          }
+          break;
+        case "like clicked":
+          {
+            $(action).removeClass("clicked");
+            const number = parseInt(stringNubmer.innerHTML);
+            stringNubmer.innerHTML = number - 1;
+          }
+          break;
+      }
+    }
+  });
 }
